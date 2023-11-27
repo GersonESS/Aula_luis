0c68579d44000bebbe3d067897894bf55e69eacc
Gerson Santos <gersones@outlook.com>
2023-11-27 20:53:28 -0300

    a

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+
+$servidor="localhost";
+$usuario="root";
+$senha="";
+$dbname="dbase";
+$conexao=mysqli_connect($servidor, $usuario, $senha, $dbname);
+if(!$conexao){
+    die(("Houve um erro : ".mysqli_connect_errno()));
+}else {
+    echo "deu certo";
+}
+
+?>
