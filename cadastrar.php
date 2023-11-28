eb453ab3413ba537b9cc2c3903518dd46c1c3b59
Gerson Santos <gersones@outlook.com>
2023-11-28 12:17:10 -0300

    a

--- cadastrar.php
+++ cadastrar.php
@@ -5,7 +5,12 @@
     $cidade=$_POST["cidade"];
     $estado=$_POST["estado"];
     $data_nasc=$_POST["data_nasc"];
-    $sql="INSERT INTO regcli (nome, salario, data_nasc, cidade, estado) VALUE ('$nome', '$salario', '$data_nasc', '$cidade', '$estado')";
+
+    $sql="INSERT INTO regcli 
+                (nome, salario, data_nasc, cidade ,estado) 
+    VALUE
+                ('$nome', '$salario', '$data_nasc', '$cidade', '$estado')";
+
 if(mysqli_query($conexao, $sql)){
     echo("<h1>Cadastrado com sucesso</h1>");
 }else{
@@ -13,3 +18,4 @@ if(mysqli_query($conexao, $sql)){
 }
 mysqli_close($conexao);
 ?>
+<!--,sexo ,peso ,altura ,nacionalidade ,hoby
